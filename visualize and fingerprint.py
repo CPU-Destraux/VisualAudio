eff58d703887313bf4c508886f93b5ccb1c2bd98
CPU-Destraux <eric.digioacchino01@gmail.com>
2017-02-18 12:42:18 -0500

    Update visualize and fingerprint.py

--- visualize and fingerprint.py
+++ visualize and fingerprint.py
@@ -13,12 +13,12 @@ import time
 import cv2
 
 CHUNK = 512 # number of data points to read at a time
-RATE = 10000 # time resolution of the recording device (Hz)
-FRAMES = 100
-LAYERS = 3
-THRESH = 0.1
+RATE = 10000 # time resolution of the recording device (Hz)... I recommend the use of 10K - 20K
+FRAMES = 100    #adjust this the change the length of time you wish to record
+LAYERS = 3      #the program has three color layers
+THRESH = 0.1    #If you can get the thresh to work, good for you!
 
-def hsv2rgb(d):
+def hsv2rgb(d): #Allows us to convert frequencues and intensity to color
     dat = []
     for h in d:
         dat.append(list(int(i * 255) for i in colorsys.hsv_to_rgb(h,1.0,1.0)))
@@ -44,12 +44,10 @@ for i in range(FRAMES):
     for j in xrange(len(data)):
         img[i][j][:3] = rgb[j][:3]
 print time.clock() - start
-#img = np.array(img * 255, dtype=np.uint8)
-#threshed = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 0)
+#img = np.array(img * 255, dtype=np.uint8)  #Good luck using this line
+#threshed = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 0)    #Throws an error that breaks the program
 img = cv2.filter2D(img,-1,kernel)
 cv2.imshow('ImageWindow',img)
-cv2.imwrite("wave_image.png", img)
-print "saved"
 cv2.waitKey(0)
 
 # close the stream gracefully
